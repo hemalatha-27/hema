ff4c9cbd5318e2c3f737a901fa003cd1749dd591
hemalatha-27 <54058635+hemalatha-27@users.noreply.github.com>
2019-08-14 22:03:18 +0530

    naturalnum.c

---
+++ naturalnum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int i,num,sum=0;
+printf("enter a number\n");
+scanf(%d",&num);
+for(i=1;i<=num;i++)
+{sum=sum+i;
+}
+printf("sum of %d natural num=%d\n",num,sum);
+return 0;
+}
