1e765a1167e9f0a764ab42f5f7cfaa13cee2782c
hemalatha-27 <54058635+hemalatha-27@users.noreply.github.com>
2019-08-14 20:29:57 +0530

    alphabet.c

---
+++ begin/set1/alphabet.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("enter a character");
+scanf("%c",&c);
+if(c>='a'&& c<='z')||(c>='A'&& c<='Z'))
+printf("%c is alphabet",c);
+else
+printf("%c is not alphabet",c);
+return 0;}
