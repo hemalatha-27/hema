6a6a8b980665ed83ad78ee8efbab672dde6f4439
hemalatha-27 <54058635+hemalatha-27@users.noreply.github.com>
2019-08-15 19:53:34 +0530

    multiple20.c

---
+++ multiple20.c
@@ -0,0 +1,14 @@
+#includ<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i;
+clrscr();
+printf("\n enter a integer number");
+scanf("%d",&n);
+for(i=1;i<=5;i++)
+{
+printf("\n%d"%d*%d=%d"n,i,n*i);
+}
+getch();
+}
