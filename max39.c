213213089c9282bca95404896b3e6c5c9598d117
hemalatha-27 <54058635+hemalatha-27@users.noreply.github.com>
2019-08-17 06:55:00 +0530

    max39.c

---
+++ max39.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a[10];
+int i;
+int maximun;
+printf("enter 10 values:");
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+maximun=a[0];
+or(i=0;i<10;i++)
+{
+if(a[i]>maximun)
+{
+maximun=a[i];
+}
+}
+printf("maximun number is:",maximum);return 0;
+}
